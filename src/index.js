25703f0a1398fac0151e68cc01d967de63415fbc
John Crisostomo <jd@snapzio.com>
2017-07-14 16:09:15 +0800

    stashing changes for now

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import jwt, { Verifier } from 'feathers-authentication-jwt';
 
 class ImpersonateVerifier extends Verifier {
-  verify(request, payload, done) {
+  async verify(request, payload, done) {
     // verify that the one requesting is an admin
     // get user from email / username / whatever
+    const requesterId = payload[`${this.options.entity}Id`];
+
+    if (!requesterId) {
+      return done(null, {}, payload);
+    }
+
+    const requester = await this.service.get(requesterId);
+
+    if (!requester) {
+      return done(null, {}, payload);
+    }
+
+    if (
+      !requester[`${this.options.rolesField}`].includes(
+        `${this.options.allowedRole}`,
+      )
+    ) {
+      return done(null, {}, payload);
+    }
+
+    // at this point request exists and has a valid roles
+    // query the desired user to be impersonated
+    // pass that user
     done(null, user, payload);
   }
 }
@@ -14,16 +37,17 @@ export default function(options) {
     entity: 'user',
     service: 'users',
     passReqToCallback: true,
+    rolesField: 'roles',
+    allowedRole: 'admin',
     jwtFromRequest: [
       ExtractJwt.fromHeader,
       ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
-      ExtractJwt.fromBodyField('body')
+      ExtractJwt.fromBodyField('body'),
     ],
     secretOrKey: auth.secret,
     session: false,
-    Verifier: ImpersonateVerifier
-};
-
+    Verifier: ImpersonateVerifier,
+  };
 
   return jwt({ ...defaultOptions, ...options });
 }
